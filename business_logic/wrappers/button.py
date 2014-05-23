4897c981ef3156f2a76dad0582c0e279bd817093
oprilepin <oleh.prilepin@gmail.com>
2014-05-23 22:45:51 +0300

    Update button.py
    Added scelet for button factory actions and declarations

--- business_logic/wrappers/button.py
+++ business_logic/wrappers/button.py
@@ -1,3 +1,25 @@
+import webdriver
+
+# Button Wrapper
 class ButtonFactory(object):
-  def button(self):
+  def _init_(self, locator, action):
+    self.locator = locator
+    self.action = action
+    
+  
+# Button Declaration
+class Button(object):
+  def _init_(self, locator):
+    self.locator = locator
+  
+  def get_button_by_xpath(self):
+    pass
+  
+  def get_button_by_id(self):
+    pass
+  
+  def get_button_by_css(self):
+    pass
+  
+  def get_button_by_class(self):
     pass
