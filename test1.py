c64b7f6847d53051d33be5a4d8daccdf6e93ae57
Oleh <oleh.prilepin@gmail.com>
2017-09-08 15:41:01 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,6 +1,15 @@
-import os
-def test1():
-  os.system("start /wait cmd /c {command}")
-  
-  print ("Started")
-test1()
+import sys
+def create():
+    print("creating new  file")
+    name=raw_input ("enter the name of file:")
+    extension=raw_input ("enter extension of file:")
+    try:
+        name=name+"."+extension
+        file=open(name,'a')
+
+        file.close()
+    except:
+            print("error occured")
+            sys.exit(0)
+
+create()
